f936db35aa679c1197718cbc69f0282fd2efa971
1017251 <1017251@CRN-323E-13-D>
2024-02-20 14:15:51 -0500

    Add project files.

---
+++ OregonWWI/FiniteStateAI.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OregonWWI
+{
+    internal class FiniteStateAI<T> where T : Enum
+    {
+        public T CurrentState { get; private set; }
+
+        private Dictionary<T, Func<StateData, StateData>> States = new();
+
+        private Func<StateData, StateData> ActiveState;
+
+        public void AddState(T Enum, Func<StateData, StateData> Update)
+            => States.Add(Enum, Update);
+
+        private StateData NowData;
+
+        public StateData Update(T newState)
+        {
+            Switch(newState);
+            NowData = ActiveState(NowData);
+            return NowData;
+        }
+
+        public void Switch(T NextState)
+        {
+            ActiveState = States.TryGetValue(NextState, out var Value) ? Value : throw new Exception("That aint a state bro");
+            CurrentState = NextState;
+        }
+    }
+}
---
+++ OregonWWI/Game1.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Linq;
+
+namespace OregonWWI
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private FiniteStateAI<States> FiniteStateAI = new();
+        private SpriteFont font;
+
+        private StateData currentState;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            GameLogic.Initalize(FiniteStateAI);
+            base.Initialize();
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            font = Content.Load<SpriteFont>("font");
+            currentState = FiniteStateAI.Update(States.ChooseCharacter);
+        }
+
+        private int TotalCharsDrawn = 0;
+        private bool acceptingInput = false;
+        private int totalTicks = 0;
+
+        protected override void Update(GameTime gameTime)
+        {
+            totalTicks++;
+#if DEBUG
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+#endif
+            if(acceptingInput)
+            {
+                //TODO: input lol
+            }
+
+            if (totalTicks % 2 == 0)
+                TotalCharsDrawn++;
+
+            base.Update(gameTime);
+        }
+        const int padding = 24;
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            int charsDrawn = 0;
+            _spriteBatch.Begin();
+            Vector2 cumulativeLocation = Vector2.One * padding;
+
+            currentState.Deconstruct(out var text, out var operations);
+            bool hasSkipped = false;
+
+            foreach (var str in text)
+            {
+                if (hasSkipped)
+                    break;
+                DrStr(str);
+            }
+
+            foreach (var str in operations.Select(o => o.OptionPrint))
+            {
+                if (hasSkipped)
+                    break;
+                DrStr(str);
+            }
+
+            void DrStr(CString str)
+            {
+                int potentialCharsDrawn = str.Length + charsDrawn;
+                if (potentialCharsDrawn > TotalCharsDrawn)
+                {
+                    str = new CString(str.Color, str.String[..(str.Length + TotalCharsDrawn - potentialCharsDrawn)]);
+                    hasSkipped = true;
+                }
+
+
+                Vector2 loc = font.MeasureString(str.String);
+                _spriteBatch.DrawString(font, str.String, cumulativeLocation, str.Color);
+
+                cumulativeLocation += new Vector2(0, loc.Y);
+                charsDrawn += str.Length;
+            }
+
+            if(totalTicks % 20 > 10)
+            {
+                Vector2 BottomLeft = new Vector2(0, _graphics.PreferredBackBufferHeight) - new Vector2(-1, 1) * padding;
+                _spriteBatch.DrawString(font, "_", BottomLeft, Color.White, 0, Vector2.Zero, 2, SpriteEffects.None, 0);
+            }
+
+            _spriteBatch.End();
+
+            if (!hasSkipped)
+            {
+                acceptingInput = true;
+            }
+            base.Draw(gameTime);
+        }
+    }
+}
